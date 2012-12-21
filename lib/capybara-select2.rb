521a88eb89312a274d03837eb4e143502c069213
William Yeung <william@tofugear.com>
2012-12-21 17:33:00 +0800

    fix file names and path to get include working properly

--- lib/capybara-select2.rb
+++ lib/capybara-select2.rb
@@ -1,2 +1,22 @@
 require "capybara-select2/version"
-require 'capybara/select2'
+
+module Capybara
+  module Select2
+    def select2(value, options={})
+      raise "Must pass a hash containing 'from'" if not options.is_a?(Hash) or not options.has_key?(:from)
+      select_name = options[:from]
+
+      select2_container=find("label:contains(\"#{select_name}\")").find(:xpath, '..').find(".select2-container")
+      PP.pp(select2_container)
+
+      select2_container.find(".select2-choice").click
+
+      find(".select2-drop li:contains(\"#{value}\")").click
+
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include Capybara::Select2
+end
--- lib/capybara-select2/version.rb
+++ lib/capybara-select2/version.rb
@@ -1,5 +1,5 @@
 module Capybara
   module Select2
-    VERSION = "0.0.1"
+    VERSION = "0.0.3"
   end
 end
