9dd254b9b58b0dc73e0340cf0d9c1108da8ec66a
William Yeung <william@tofugear.com>
2013-04-21 16:14:07 +0800

    use xpath to make sure searching for select2 element is from document body

--- lib/capybara-select2.rb
+++ lib/capybara-select2.rb
@@ -10,7 +10,7 @@ module Capybara
       select2_container=first("label", text: select_name).find(:xpath, '..').find(".select2-container")
       select2_container.find(".select2-choice").click
 
-      find(".select2-drop li", text: value).click
+      find(:xpath, "//body").find(".select2-drop li", text: value).click
     end
   end
 end
