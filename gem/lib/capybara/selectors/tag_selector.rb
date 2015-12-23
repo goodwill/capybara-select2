9896ff772487a5e4117fc8edc5606a87c4421d04
Kesha Antonov <innokenty.longway@gmail.com>
2015-12-23 13:13:52 +0300

    tag_selector: use 4.0.0 syntax

--- gem/lib/capybara/selectors/tag_selector.rb
+++ gem/lib/capybara/selectors/tag_selector.rb
@@ -6,11 +6,42 @@ module Capybara
           find(:css, options[:css])
         else
           select_name = options[:from]
-          find("label", text: select_name).find(:xpath, '..').find(".select2-container")
+          find('label', text: select_name).find(:xpath, '..').find('.select2-container')
         end
-        
-        find('.select2-drop li', text: value).click
+
+        select2_container.find('.select2-selection').click
+        find(:xpath, "//body").find(".select2-search input.select2-search__field").set( value )
+
+        @drop_container = ".select2-dropdown"
+
+        select_option(value)
       end
+
+      private
+
+        def select_option(value)
+          clicked = wait_for_option_with_text(value)
+          unless clicked
+            find(:xpath, "//body").find(select2_option_selector, text: value).click
+          end
+        end
+
+        def select2_option_selector
+          "#{@drop_container} li.select2-results__option"
+        end
+
+        def wait_for_option_with_text(value)
+          clicked = false
+          begin
+            Timeout.timeout(2) do
+              sleep(0.1) unless page.has_selector?(select2_option_selector, text: value)
+            end
+          rescue TimeoutError
+            find(:xpath, "//body").find(select2_option_selector, text: value).click
+            clicked = true
+          end
+          clicked
+        end
     end
   end
 end
