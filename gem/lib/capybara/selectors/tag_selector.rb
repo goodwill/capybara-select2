8be6ea91d0b99552b35367f7918b4161e9668792
Kesha Antonov <innokenty.longway@gmail.com>
2016-03-09 21:35:28 +0300

    fix methods override

--- gem/lib/capybara/selectors/tag_selector.rb
+++ gem/lib/capybara/selectors/tag_selector.rb
@@ -14,37 +14,37 @@ module Capybara
 
         @drop_container = ".select2-dropdown"
 
-        select_option(value, options[:first])
+        select_tag_option(value, options[:first])
       end
 
       private
 
-        def select_option(value, first)
-          clicked = wait_for_option_with_text(value, first)
+        def select_tag_option(value, first)
+          clicked = wait_for_tag_option_with_text(value, first)
           unless clicked
             if first
-              find(:xpath, "//body").first(select2_option_selector, text: value).click
+              find(:xpath, "//body").first(select2_tag_option_selector, text: value).click
             else
-              find(:xpath, "//body").find(select2_option_selector, text: value).click
+              find(:xpath, "//body").find(select2_tag_option_selector, text: value).click
             end
           end
         end
 
-        def select2_option_selector
+        def select2_tag_option_selector
           "#{@drop_container} li.select2-results__option"
         end
 
-        def wait_for_option_with_text(value, first)
+        def wait_for_tag_option_with_text(value, first)
           clicked = false
           begin
             Timeout.timeout(2) do
-              sleep(0.1) unless page.has_selector?(select2_option_selector, text: value)
+              sleep(0.1) unless page.has_selector?(select2_tag_option_selector, text: value)
             end
           rescue TimeoutError
             if first
-              find(:xpath, "//body").first(select2_option_selector, text: value).click
+              find(:xpath, "//body").first(select2_tag_option_selector, text: value).click
             else
-              find(:xpath, "//body").find(select2_option_selector, text: value).click
+              find(:xpath, "//body").find(select2_tag_option_selector, text: value).click
             end
             clicked = true
           end
